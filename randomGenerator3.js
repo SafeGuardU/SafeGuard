c53a0b58a640137a3de441e201b6a5a48309776c
ArrakianSpice <aaron007duncan@gmail.com>
2024-03-02 22:46:49 +1100

    JS password generator

---
+++ randomGenerator3.js
@@ -0,0 +1,17 @@
+/* if we do end up programming some things in JS it can be convenient to 
+use an online complier instead of mucking around to get it working in your own environment every time */ 
+
+function generateRandomPassword(length) {
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    return password;
+  }
+  
+  // Example usage: generate a random password with length 12
+  var newPassword = generateRandomPassword(15);
+  console.log(newPassword);
+  
