cda60cc49af4fad7d0353ae42aef491eada06f79
ArrakianSpice <aaron007duncan@gmail.com>
2024-03-03 21:18:51 +1100

    UI

--- randomGenerator4.py
+++ randomGenerator4.py
@@ -9,10 +9,21 @@ root.geometry("500x300")
 my_password = chr(randint(33,126))
 
 def new_rand():
-    pass
+    pw_entry.delete(0, END)
+
+    pw_length = int(my_entry.get())
+
+    my_password = ''
+
+    for x in range(pw_length):
+        my_password += chr(randint(33,126))
+
+    pw_entry.insert(0, my_password)
 
 def clipper():
-    pass
+    root.clipboard_clear()
+
+    root.clipboard_append(pw_entry.get())
 
 lf = LabelFrame(root, text="How many Characters?")
 lf.pack(pady=20)
@@ -20,7 +31,7 @@ lf.pack(pady=20)
 my_entry = Entry(lf, font=("Helvetica", 24))
 my_entry.pack(pady=20, padx=20)
 
-pw_entry = Entry(root, text="", font=("Helvetica", 24))
+pw_entry = Entry(root, text="", font=("Helvetica", 24), bd=0, bg="SystemButtonFace")
 pw_entry.pack(pady=20)
 
 
