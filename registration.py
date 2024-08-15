dae09c8807560a2ef7f2da824b81fb3502eeb23a
Lowkeyplaya <162514265+Lowkeyplaya@users.noreply.github.com>
2024-08-15 10:24:38 +1000

    Update registration.py

--- registration.py
+++ registration.py
@@ -13,17 +13,17 @@ def create_user(conn, username, master_password):
 
     if count > 0:
         print("Username already exists. Please choose a different username.")
-        return # Exit the function if the username is already taken
+        return #Return the warning message if the username is already taken
 
     #Generates a unique salt for the user
     salt = generate_salt()
     master_password_hash = hash_password(master_password, salt) #Hashes the master password using the generated salt
 
-    # Inserts the new user into the database with the hashed password and salt
+    #Registers the new user into the database with the hashed password and salt
     cursor.execute('''
     INSERT INTO Users (Username, MasterPasswordHash, MasterPasswordSalt)
     VALUES (?, ?, ?)
     ''', (username, base64.b64encode(master_password_hash).decode(), base64.b64encode(salt).decode()))
-    #Save the changes to the database
+    #Saves the changes to the database
     conn.commit()
     print("User created successfully.")
